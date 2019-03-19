a2008b845307e14c51848178a897ebdb2736f3c0
Matt <n.matt30@yahoo.com>
2019-03-18 22:46:43 -0700

    reworked footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,71 +9,37 @@ class Footer extends React.Component {
             <div className="footer">
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-4 col-sm-12 text-left">
-                            <div className="row phone">
-                                <div className="col-xs-12">
-                                    <p>
-                                        <span className="glyphicon glyphicon-earphone glyphicon-style"></span>
-                                        <span className="phone-style">858.658.0691</span>
-                                    </p>
-                                </div>
+                        <div className="col-sm-2 col-xs-12 text-left">
+                            <div className="phone">
+                                <p>
+                                    <span className="glyphicon glyphicon-earphone glyphicon-style"></span>
+                                    <span className="phone-style">858.658.0691</span>
+                                </p>
                             </div>
-                            <div className="row address">
-                                <div className="col-xs-12">
-                                    <span className="glyphicon glyphicon-map-marker glyphicon-style marker-style"></span>
-                                    <div className="address-block">
-                                        <p>
-                                            <span className="address-style">6255 Lusk Blvd Suite 250</span>
-                                        </p>
-                                        <p>
-                                            <span className="address-style">San Diego, CA 92121</span>
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                                <div className="row">
-                                    <div className="col-xs-4">
-                                        <img src={instagram} className="social-media-icon" alt="instagram"/>
-                                    </div>
-                                    <div className="col-xs-4">
-                                        <a href="https://www.facebook.com/ucare.dental.7" target="_blank" rel="noopener noreferrer">
-                                        <img src={facebook} className="social-media-icon" alt="facebook"/>
-                                        </a>
-                                    </div>
-                                    <div className="col-xs-4">
-
-                                    </div>
-                                </div>
                         </div>
-                        <div className="col-md-8 col-sm-12">
-                            <div className="row text-center">
-                                <h1>
-                                    Contact Us
-                                </h1>
-                            </div>
-                            <div className="row">
-                                <div className="col-sm-6 col-xs-12">
-                                    <div>
-                                        <input type="text" className="form-input" placeholder="NAME"/>
-                                    </div>
-                                    <div>
-                                        <input type="text" className="form-input" placeholder="EMAIL"/>
-                                    </div>
-                                    <div>
-                                        <input type="text" className="form-input" placeholder="PHONE"/>
-                                    </div>
+                        <div className="col-sm-6 col-xs-12 text-left">
+                            <div className="address">
+                                <span className="glyphicon glyphicon-map-marker glyphicon-style marker-style"></span>
+                                <div className="address-block">
+                                    <p>
+                                        <span className="address-style">6255 Lusk Blvd Suite 250, San Diego, CA 92121</span>
+                                    </p>
+                                    {/*<p>*/}
+                                        {/*<span className="address-style">San Diego, CA 92121</span>*/}
+                                    {/*</p>*/}
                                 </div>
-                                <div className="col-sm-6 col-xs-12">
-                                    <textarea className="form-input-message" rows="5" placeholder="MESSAGE">
+                            </div>
+                        </div>
+                        <div className="col-sm-2">
 
-                                    </textarea>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col-xs-12">
-                                    <button type="submit" className="btn btn-contact">Send</button>
-                                </div>
-                            </div>
+                        </div>
+                        <div className="col-sm-1 col-xs-6">
+                            <img src={instagram} className="social-media-icon" alt="instagram"/>
+                        </div>
+                        <div className="col-sm-1 col-xs-6">
+                            <a href="https://www.facebook.com/ucare.dental.7" target="_blank" rel="noopener noreferrer">
+                                <img src={facebook} className="social-media-icon" alt="facebook"/>
+                            </a>
                         </div>
                     </div>
                 </div>
